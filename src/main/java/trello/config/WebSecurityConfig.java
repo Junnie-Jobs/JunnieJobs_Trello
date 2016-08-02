a21532d667bd04f1092def5c2e9764b55cd6c762
Junnie-Jobs <dj0999@naver.com>
2016-08-02 13:28:09 +0900

    WebsecurityConfig update

--- src/main/java/trello/config/WebSecurityConfig.java
+++ src/main/java/trello/config/WebSecurityConfig.java
@@ -41,15 +41,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	
         http
             .authorizeRequests()
-//            	.antMatchers("/").permitAll()
-//            	.antMatchers("/users/**").anonymous()
-            	.antMatchers("/**").permitAll()
-//            	.antMatchers("/users/**").permitAll()
-//            	.anyRequest().authenticated()
-            	//signin 페이지를 permitAll() 로 주지 않고 anonymous() 주었다.
-            	//이유는 permitAll()을 했을 경우 sign in 에 성공한 유저가 sign in form 페이지 URL 을 기억했다가 직접 access 할 경우 sign in form 페이지가 보이게 된다. 
-            	//이렇게 되는 것을 원치 않고 단지 sign in form 페이지는 authentication 이 없는 사용자들, 즉 sign in을 아직 하지 않은 사용자들만 보이게 하기 위해서다. 
-            	//그리고 나머지 모든 페이지에 대해서는 anyRequest().authenticated() 로 설정했다.           	
+            	.antMatchers("/").permitAll()
+            	.antMatchers("/users/login").anonymous()
+            	.antMatchers("/users/signUp").anonymous()
+            	.anyRequest().authenticated()    	
                 .and()
             .formLogin() //form 로그인 페이지는 아래의 경로로 이동한다.
                 .loginPage("/users/login")
@@ -66,7 +61,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             http.httpBasic();
         
          }
-    
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -91,29 +85,3 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 
-
-//@Autowired
-//public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//  auth
-//      .inMemoryAuthentication()
-//          .withUser("user").password("password").roles("USER");
-//}
-
-
-//
-//.authorizeRequests()
-//.antMatchers("/js/**", "/css/**")
-//.antMatcherS("/users/signUp**", "/users/login").permiAll();
-//
-//.antMatchers("/").access("hasRole('ROLE_USER')")
-//.antMatchers("/b/**").acceess("hasRole('ROLE_USER')")
-//
-//.formLogin()
-//	.loginPage("/users/login")
-//	.loginProcessingUrl("/users/login");
-//	.permitAll()
-//	.and()
-
-
-//.antMatchers("/boards/**").authenticated() // 이 url에 대해서는 권한을 요구 한다.
-//.anyRequest().permitAll() // 나머지 url에 대해서는 permitAll

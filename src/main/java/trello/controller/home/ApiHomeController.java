26917f44308ef2ebd05c43aa139ccc2c2af6cf22
Junnie-Jobs <dj0999@naver.com>
2016-08-18 10:06:40 +0900

    url update

---
+++ src/main/java/trello/controller/home/ApiHomeController.java
@@ -0,0 +1,23 @@
+package trello.controller.home;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Maps;
+
+@RestController
+public class ApiHomeController {
+	
+	@RequestMapping("/servers/info")
+	public Map<String, Object> info(HttpServletRequest request) {
+		Map<String, Object> infos = Maps.newHashMap();
+		infos.put("ServerName", request.getLocalName());
+		infos.put("Port", request.getLocalPort());
+		return infos;
+	}
+
+}
